bd6500730ea3cde3302337e70b80d3f24bc7ae74
valentinogarcia <81708746+valentinogarcia@users.noreply.github.com>
2023-11-20 18:06:01 -0300

    Add img to model

--- models/elemento.ts
+++ models/elemento.ts
@@ -3,11 +3,12 @@ import { tag } from "./tag";
 
 export class elemento{
     nombre: String;
+    img:String;
     tags: Array<tag>;
     stats: JSON;
     _id:ObjectId;
 
-    constructor(inNombre: String, inTags: Array<tag>,inStats:JSON,_id:ObjectId|string|null){
-        this.nombre = inNombre; this.tags = inTags ; this.stats=inStats;if(typeof(_id)=="string"){this._id=new ObjectId(_id)}else{;this._id=_id;}
+    constructor(inNombre: String, inTags: Array<tag>,inStats:JSON,img:String,_id:ObjectId|string|null){
+        this.nombre = inNombre; this.tags = inTags ; this.stats=inStats;this.img=img;if(typeof(_id)=="string"){this._id=new ObjectId(_id)}else{;this._id=_id;}
     }
 }
